d6c97833ef3a181cfe2c2c64686b44646549da1f
barestu <fheartnet@gmail.com>
2018-02-24 16:19:54 +0700

    menambahkan file exercises 8-9, rename exercises 4

---
+++ p0-w5-e9-cari-pelaku.js
@@ -0,0 +1,19 @@
+function cariPelaku(str) {
+  // you can only write your code here!
+  var hasil = str.match(/abc/g);
+  //console.log(hasil);
+  
+  return hasil.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
+
+/*
+Problem
+Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. Gunakan regex untuk melatih kemampuan regex.
+*/
